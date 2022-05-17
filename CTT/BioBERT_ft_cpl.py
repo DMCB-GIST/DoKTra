9742bdcccd520f18c08604fed2917a8cb84b2882
DonghaChoiGIST <101086211+DonghaChoiGIST@users.noreply.github.com>
2022-05-17 17:45:16 +0900

    Add files via upload

--- CTT/BioBERT_ft_cpl.py
+++ CTT/BioBERT_ft_cpl.py
@@ -118,19 +118,19 @@ label_list_dict = {'chemprot' : ["CPR:3", "CPR:4", "CPR:5", "CPR:6", "CPR:9", "f
 
 # dataset_dict = dataset_dict_loader(args.input_dir,label_list_dict[args.task])
 
-if args.task = 'chemprot':
+if args.task == 'chemprot':
     dataset_dict = dataset_dict_loader(args.input_dir,label_list_dict[args.task])
     config = transformers.BertConfig.from_pretrained(args.config_file, num_labels=len(label_list_dict[args.task]))
 
-if args.task = 'gad':
+if args.task == 'gad':
     dataset_dict = dataset_dict_loader(args.input_dir,label_list_dict[args.task],dev_header=True,tst_header=False)
     config = transformers.BertConfig.from_pretrained(args.config_file, num_labels=len(label_list_dict[args.task]))
 
-if args.task = 'ddi':
+if args.task == 'ddi':
     dataset_dict = dataset_dict_loader(args.input_dir,label_list_dict[args.task],trn_header=True,dev_header=True)
     config = transformers.BertConfig.from_pretrained(args.config_file, num_labels=len(label_list_dict[args.task]))
 
-if args.task = 'i2b2':
+if args.task == 'i2b2':
     dataset_dict = dataset_dict_loader(args.input_dir,label_list_dict[args.task],trn_header=True,dev_header=True)
     config = transformers.BertConfig.from_pretrained(args.config_file, num_labels=len(label_list_dict[args.task]))
 
